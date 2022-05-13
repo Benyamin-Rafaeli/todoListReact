9a7ae24b8fafd3ea0019ea152fa09e998b16c278
boka <beni.r@panorays.com>
2022-05-13 15:16:53 +0300

    fix: added scss basic styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './views/App';
-
+import './views/styles/commom.scss'
+import './views/styles/reset.scss'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
