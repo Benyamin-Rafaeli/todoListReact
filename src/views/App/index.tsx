0c6e861504d5d797b15def34c089bbea28f3f01a
boka <beni.r@panorays.com>
2022-05-13 22:42:26 +0300

    fix: event preparation onAdd

--- src/views/App/index.tsx
+++ src/views/App/index.tsx
@@ -1,4 +1,5 @@
 import { useToDoStore } from '../../data/stores/useToDoStore';
+import { InputPlus } from '../components/inputPlus';
 import styles from './index.module.scss';
 import React, { useEffect } from 'react';
 
@@ -10,14 +11,21 @@ export const App: React.FC = () => {
     state.removeTask,
   ]);
 
-  useEffect(() => createTask('idjshgiu'), []);
-
-  console.log(111, tasks);
+  useEffect(() => {
+    createTask('idjshgiu');
+  }, []);
 
   return (
     <article className={styles.article}>
       <h1 className={styles.articleTitle}>To Do App</h1>
       <section className={styles.articleSection}></section>
+      <InputPlus
+        onAdd={title => {
+          if (title) {
+            createTask(title);
+          }
+        }}
+      />
       <section className={styles.articleSection}></section>
     </article>
   );
---
+++ src/views/components/inputPlus/index.tsx
@@ -0,0 +1,10 @@
+import styles from './index.module.scss';
+import React from 'react';
+
+interface InputPlusProps {
+  onAdd: (title: string) => void;
+}
+
+export const InputPlus: React.FC<InputPlusProps> = ({ onAdd }) => {
+  return <div>1231</div>;
+};
