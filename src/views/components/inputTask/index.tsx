ea7c235167ac4accf637c399b7639b85923e69fb
boka <beni.r@panorays.com>
2022-05-14 18:41:04 +0300

    fix: inputForm

--- src/views/components/inputTask/index.tsx
+++ src/views/components/inputTask/index.tsx
@@ -10,5 +10,34 @@ interface InputTaskProps {
 }
 
 export const InputTask: React.FC<InputTaskProps> = ({ id, title, onDone, onEdited, onRemoved }) => {
-  return <div className={styles.inputTask}>{title}</div>;
+  const [checked, setChecked] = useState(false);
+
+  return (
+    <div className={styles.inputTask}>
+      <label>
+        <input
+          type="checkbox"
+          checked={checked}
+          className={styles.inputTaskCheckbox}
+          onChange={evt => {
+            setChecked(evt.target.checked);
+            if (evt.target.checked) {
+              onDone(id);
+            }
+          }}
+        />
+        <h3 className={styles.inputTaskTitle}></h3>
+      </label>
+      <button aria-label="Edit" className={styles.inputTaskEdit} onClick={() => {}} />
+      <button
+        aria-label="Remove"
+        className={styles.inputTaskRemove}
+        onClick={() => {
+          if (confirm('Are you sure?')) {
+            onRemoved(id);
+          }
+        }}
+      />
+    </div>
+  );
 };
